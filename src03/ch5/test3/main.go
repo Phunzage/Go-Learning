dcfadedfc79e923094d5993a2ef7a63d8aa13089
Phunzage <3143884145@qq.com>
2025-04-13 23:06:38 +0800

    third commit

--- src03/ch5/test3/main.go
+++ src03/ch5/test3/main.go
@@ -14,35 +14,49 @@ import (
 3.展示所有学员信息
 */
 
-func showMenu() {
-	fmt.Println("欢迎来到学员信息管理系统")
-	fmt.Println("1. 添加学员信息")
-	fmt.Println("2. 编辑学员信息")
-	fmt.Println("3. 展示所有学员信息")
-	fmt.Println("4. 退出系统")
+// 存在的问题
+// 1.student.go中全局切片 studentSlice，存在数据竞争风险且封装性差。
+// 2.编辑时用索引 i-1，但ID可能不连续。
+// 3.直接打印结构体，可读性差。
+// 4.default 分支直接退出，应明确处理选项4。
+
+func ShowMenu() {
+	fmt.Println(`
+	1. 添加学员信息
+	2. 编辑学员信息
+	3. 展示所有学员信息
+	4. 退出系统
+	`)
 }
 
 func main() {
-	showMenu()
-	var a int8
-	fmt.Print("请输入你的选项: ")
-	fmt.Scan(&a)
-	switch a {
-	case 1:
-		addStudent()
-	case 2:
-		editStudent()
-	case 3:
-		showStudent()
-	default:
-		fmt.Println("您已退出")
-		os.Exit(0)
+	fmt.Println("欢迎来到学员信息管理系统")
+	for {
+		ShowMenu()
+		var a int
+		fmt.Print("请输入你的选项: ")
+		fmt.Scan(&a)
+		switch a {
+		case 1:
+			addStudent()
+			fmt.Println("添加成功")
+		case 2:
+			editStudent()
+			fmt.Println("修改成功")
+		case 3:
+			fmt.Println("当前存储的学生信息有：")
+			fmt.Println("id name class")
+			st.ShowStudent()
+		default:
+			fmt.Println("您已退出")
+			os.Exit(0)
+		}
 	}
 }
 
-func addStudent() int8 {
+func addStudent() {
 	var (
-		id          int8
+		id          int
 		name, class string
 	)
 	fmt.Println("请输入学员信息")
@@ -53,14 +67,20 @@ func addStudent() int8 {
 	fmt.Print("请输入学员class: ")
 	fmt.Scan(&class)
 	stu := st.NewStudent(id, name, class)
-	fmt.Println(*stu)
-	return 0
+	st.SaveStudent(stu)
 }
 
 func editStudent() {
-	//编辑学员信息
-
-}
-func showStudent() {
+	var (
+		i           int
+		name, class string
+	)
+	fmt.Print("请输入要修改的学生id：")
+	fmt.Scan(&i)
+	fmt.Printf("请输入%d号学生修改后的姓名：", i)
+	fmt.Scan(&name)
+	fmt.Printf("请输入%d号学生修改后的班级：", i)
+	fmt.Scan(&class)
+	st.ChangeStudent(i-1, name, class)
 
 }
--- src03/ch5/test3/student/student.go
+++ src03/ch5/test3/student/student.go
@@ -1,15 +1,34 @@
 package st
 
-type Student struct {
-	Id    int8
-	Name  string
-	Class string
+import "fmt"
+
+type student struct {
+	id    int
+	name  string
+	class string
 }
 
-func NewStudent(id int8, name, class string) *Student {
-	return &Student{
-		Id:    id,
-		Name:  name,
-		Class: class,
+var studentSlice []student
+
+func SaveStudent(x *student) {
+	studentSlice = append(studentSlice, *x)
+}
+
+func ShowStudent() {
+	for _, v := range studentSlice {
+		fmt.Println(v)
+	}
+}
+
+func ChangeStudent(i int, name, class string) {
+	studentSlice[i].name = name
+	studentSlice[i].class = class
+}
+
+func NewStudent(id int, name, class string) *student {
+	return &student{
+		id:    id,
+		name:  name,
+		class: class,
 	}
 }
