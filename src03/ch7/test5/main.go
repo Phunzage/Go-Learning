831f91bec5c77f0165810e492e559a77b7c5bff8
Phunzage <3143884145@qq.com>
2025-04-24 08:53:18 +0800

    nineteenth commit

--- src03/ch7/test5/main.go
+++ src03/ch7/test5/main.go
@@ -1,5 +1,31 @@
+// 文件操作4
+
+// 复制文件
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+// 方式一，将原文件中的数据读取出来，然后写入目标文件中
+
+func RWFiles() {
+	// 从源文件中读取数据
+	data, err := os.ReadFile("from.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// 写入目标文件
+	err = os.WriteFile("to.txt", data, 0666)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("复制成功")
+	}
+}
+
+func main() {
+	RWFiles()
 }
