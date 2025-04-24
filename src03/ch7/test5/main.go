c9d6b21b55eca0da7e0981831d3a2aa6746307c1
Phunzage <3143884145@qq.com>
2025-04-24 09:18:56 +0800

    twentieth commit

--- src03/ch7/test5/main.go
+++ src03/ch7/test5/main.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -26,6 +27,37 @@ func RWFiles() {
 	}
 }
 
+// 方法二，使用io.Copy，一边读一边写，先将内容读到缓冲区中，再写入到目标文件中，缓冲区默认大小为 32KB。
+
+func IoCopy() {
+	// 以只读的方式打开源文件
+	from, err := os.OpenFile("from.txt", os.O_RDONLY, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer from.Close()
+
+	// 以只写的方式打开目标文件
+	to, err := os.OpenFile("to.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer to.Close()
+
+	// 复制
+	written, err := io.Copy(to, from)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		// 输出22，在Windows环境下，若文件以文本模式打开，\r\n会被转换为\n读取，再转换回\r\n写入。Go的os.OpenFile默认使用二进制模式，不进行换行符转换，直接按原样复制字节
+		// 所以：前两行每行占6（数字）+2（换行符）=8字节，共16字节。第三行仅6字节。总计：16 + 6 = 22字节
+		fmt.Println(written)
+	}
+}
+
 func main() {
-	RWFiles()
+	// RWFiles()
+	IoCopy()
 }
