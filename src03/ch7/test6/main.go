67b8d79c44f9362c70272dd53fa78a18ec04e5c5
Phunzage <3143884145@qq.com>
2025-04-27 23:05:05 +0800

    twenty-first commit

---
+++ src03/ch7/test6/main.go
@@ -0,0 +1,34 @@
+// 文件操作5
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// 重命名文件
+func rename() {
+	err := os.Rename("MV.txt", "mv.txt")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("重命名成功")
+	}
+}
+
+// 删除文件
+func remove() {
+	// 删除当前目录下所有的文件与子目录
+	err := os.Remove("move.txt")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("删除成功")
+	}
+}
+
+func main() {
+	rename()
+	remove()
+}
