a0ac4e85cd3c65baeee9b14e42b75402409099af
phunzage <3143884145@qq.com>
2026-02-05 11:42:13 +0800

    test

--- testGo/test000/main.go
+++ testGo/test000/main.go
@@ -2,6 +2,12 @@
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	a := [6]int{1, 2, 3, 4, 5, 6}
+	// for i := range 7 {
+	//     fmt.Scan(&a[i])
+	// }
+	fmt.Println(a[:3], a[3:6])
 }
